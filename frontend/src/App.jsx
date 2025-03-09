2dbd1e89d11b7aa5948813289a92212568bc4ef5
Navneet Vishwakarma <rajvl132011@gmail.com>
2025-03-09 12:48:12 +0530

    I don't know this is correct or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Doctors from './pages/Doctors'
-import Login from './pages/Login'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Appointment from './pages/Appointment'
-import MyAppointments from './pages/MyAppointments'
-import MyProfile from './pages/MyProfile'
-import Footer from './components/Footer'
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Doctors from './pages/Doctors';
+import Login from './pages/Login';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Appointment from './pages/Appointment';
+import MyAppointments from './pages/MyAppointments';
+import MyProfile from './pages/MyProfile';
+import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Verify from './pages/Verify'
-import ResetPassword from './pages/ResetPassword'
+import Verify from './pages/Verify';
+import ResetPassword from './pages/ResetPassword';
+
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
@@ -30,11 +31,11 @@ const App = () => {
         <Route path='/my-appointments' element={<MyAppointments />} />
         <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/verify' element={<Verify />} />
-        <Route path='/reset-password/:token' element={<ResetPassword />} />
+        <Route path='/reset-password/:userId' element={<ResetPassword />} /> {/* Updated to userId */}
       </Routes>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
