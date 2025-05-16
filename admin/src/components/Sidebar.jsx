2b69fc658b107c87b9c1134a1dbeae74426aa2a0
Navneet Vishwakarma <rajvl132011@gmail.com>
2025-05-17 00:26:30 +0530

    correct

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
 
   return (
     <div className='min-h-screen bg-rose-100 border-r'>
-      {aToken && <ul className='text-[#515151] mt-5'>
+      {aToken && <ul className='text-[#515151] mt-5'> 
 
         <NavLink to={'/admin-dashboard'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-rose-600' : ''}`}>
           <img className='min-w-5' src={assets.home_icon} alt='' />
